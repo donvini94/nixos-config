7e6194dea4f186b971e426efd25019d567ca0ff0
Vincenzo Pace <vincenzo.pace@mailbox.org>
2024-01-19 20:47:17 +0100

    add airpods

--- configuration.nix
+++ configuration.nix
@@ -35,6 +35,14 @@
   hardware.bluetooth.enable = true;
   security.rtkit.enable = true;
 
+  # Comment this in the first time you want to connect to AirPods.
+  # In order to connect, you have to press the button on the back
+  # of the AirPods case.
+  # `breder` is only needed for the initial connection of the AirPods.
+  # Afterwards the mode can be relaxed to `dual` (the default) again.
+  #
+  # hardware.bluetooth.settings = { General = { ControllerMode = "bredr"; }; };
+
   users.users.vincenzo = {
     isNormalUser = true;
     description = "Vincenzo Pace";
