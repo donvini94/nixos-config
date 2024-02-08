1985bbd6514eff83a454dfc9d906eff59a1405bd
Vincenzo Pace <vincenzo.pace@mailbox.org>
2024-02-08 11:31:32 +0100

    adapt downstream namechange

--- modules/services.nix
+++ modules/services.nix
@@ -7,8 +7,8 @@
     printing.enable = true;
     xserver = {
       enable = true;
-      layout = "us";
-      xkbVariant = "";
+      xkb.layout = "us";
+      xkb.variant = "";
     };
     pipewire = {
       enable = true;
