0b8a19f28cf0c05a70d7e4459524d1ea29dd28ce
Vincenzo Pace <pace@amiconsult.de>
2024-05-19 20:12:43 +0200

    dockerRegistry

--- hosts/server/alucard.nix
+++ hosts/server/alucard.nix
@@ -1,9 +1,24 @@
-{ inputs, config, lib, pkgs, modulesPath, ... }:
+{
+  inputs,
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}:
 
 {
-  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ./disk-config.nix ];
-  boot.initrd.availableKernelModules =
-    [ "ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk" ];
+  imports = [
+    (modulesPath + "/profiles/qemu-guest.nix")
+    ./disk-config.nix
+  ];
+  boot.initrd.availableKernelModules = [
+    "ata_piix"
+    "uhci_hcd"
+    "virtio_pci"
+    "sr_mod"
+    "virtio_blk"
+  ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
@@ -16,7 +31,14 @@
   # networking.interfaces.ens3.useDHCP = lib.mkDefault true;
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [ 80 443 8096 22 58080 5050];
+    allowedTCPPorts = [
+      80
+      443
+      8096
+      22
+      58080
+      5050
+    ];
   };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
@@ -27,17 +49,22 @@
 
   users.users.vincenzo = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "docker" ];
+    extraGroups = [
+      "wheel"
+      "docker"
+    ];
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDj2dxqVTVKEzFUBw4Ol8G2XBwTHEwxFquzYNIelKBOzDQLE2WNvjfBko1iQkUNHrVfr3GiAuxdsE+O3hltDvo4UsQqsEqCSu/HEWRfyXDJrcLSm7ogkOAGBZtrmIr73YfGhpzRqcfnoAqSJOkX6PFmaFJ+YgoOuJLH6KbQo3xv0r5RqFkZhfnOiD5gwMtEExP4uawycb9mrsqxOWoMANR870qYq6JERxcGZU4m0UcvnpB01EbvTuWMIACL11cCylkcCPoDnv9KD94k0nhqGOE5/UB6mxRPBBJdQk3Dd3KXe2u2s++Enpu2WKdqOFywxxvXZ2PHBh4Oy8eJpytzMWxSUcLNcNk54JgAgaUCYYN0s3CmKh2r+z6pGSo5xUuJxyl13TfsSzCTsx3dkUOAaRpQAIHDseIDKX983zDS831GZA1d4xiOOtC7ct8F8z3qMokHU+N8OB0ys2T7cS29Q9BKSgaPSBlM0YTWQwD0R2Tf+D74VQnSYPgNNGFByZwV8bc= vincenzo@nixos"
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCPlhh/4miDPy8MD7ckimo0ZlEyRIIQymAeNrpvbURg+H0G+kztFgr2x0muzAVwy5noz7511zQBkG9q+lJfWHzjGvVibew5HhcdlECkzpStrkRkupM0l7Ql1ILlQb/lME1v4TM+JM1nCbOgIqkjKJ/dzE3WqHz8CfJ6ilf5QedKHnAFbMu6miOGHMJxDje+0t/51QPul513d2oyIjtUBjtW0Yo77PgSuopFbhEI//cn0P7QVJArbmv7YZqGNifVzMyzQBlvXQtJC0CR/bGTJwspCCU2xIangzHrkKxRqkZJrk1zC5JyMbW1oRUZ3ah7MbUq/ivAUfjvzvkrZS5DbigMmSIbGmoK9d/k6pQjj4gyL1Q5KZRq4g2JKkV6Uhaqr2yfG2F0T6FGKnhGO6P5PK2bkAobfCfLL5IGkceK/WB0InMKfdbii971CeUY0qk+1ad7Fn9txuR5omttkEtM9Hh9Afz1kGxa4ia9+d71OV4KoXVykqr/bD284rhOooX4/mU= vincenzo@dracula"
     ];
-
   };
 
   users.users.kyrill = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "docker" ];
+    extraGroups = [
+      "wheel"
+      "docker"
+    ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJtziDwyaqKfBPL1dDEM6kMdA+KTL+d0810PzAbOsWHn kyrill@kyrill-ThinkPad-T495"
     ];
@@ -45,7 +72,10 @@
 
   users.users.overleaf = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "docker" ];
+    extraGroups = [
+      "wheel"
+      "docker"
+    ];
   };
 
   users.users.git = {
@@ -55,13 +85,15 @@
 
   users.users.jellyfin = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "docker" ];
+    extraGroups = [
+      "wheel"
+      "docker"
+    ];
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDj2dxqVTVKEzFUBw4Ol8G2XBwTHEwxFquzYNIelKBOzDQLE2WNvjfBko1iQkUNHrVfr3GiAuxdsE+O3hltDvo4UsQqsEqCSu/HEWRfyXDJrcLSm7ogkOAGBZtrmIr73YfGhpzRqcfnoAqSJOkX6PFmaFJ+YgoOuJLH6KbQo3xv0r5RqFkZhfnOiD5gwMtEExP4uawycb9mrsqxOWoMANR870qYq6JERxcGZU4m0UcvnpB01EbvTuWMIACL11cCylkcCPoDnv9KD94k0nhqGOE5/UB6mxRPBBJdQk3Dd3KXe2u2s++Enpu2WKdqOFywxxvXZ2PHBh4Oy8eJpytzMWxSUcLNcNk54JgAgaUCYYN0s3CmKh2r+z6pGSo5xUuJxyl13TfsSzCTsx3dkUOAaRpQAIHDseIDKX983zDS831GZA1d4xiOOtC7ct8F8z3qMokHU+N8OB0ys2T7cS29Q9BKSgaPSBlM0YTWQwD0R2Tf+D74VQnSYPgNNGFByZwV8bc= vincenzo@nixos"
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCPlhh/4miDPy8MD7ckimo0ZlEyRIIQymAeNrpvbURg+H0G+kztFgr2x0muzAVwy5noz7511zQBkG9q+lJfWHzjGvVibew5HhcdlECkzpStrkRkupM0l7Ql1ILlQb/lME1v4TM+JM1nCbOgIqkjKJ/dzE3WqHz8CfJ6ilf5QedKHnAFbMu6miOGHMJxDje+0t/51QPul513d2oyIjtUBjtW0Yo77PgSuopFbhEI//cn0P7QVJArbmv7YZqGNifVzMyzQBlvXQtJC0CR/bGTJwspCCU2xIangzHrkKxRqkZJrk1zC5JyMbW1oRUZ3ah7MbUq/ivAUfjvzvkrZS5DbigMmSIbGmoK9d/k6pQjj4gyL1Q5KZRq4g2JKkV6Uhaqr2yfG2F0T6FGKnhGO6P5PK2bkAobfCfLL5IGkceK/WB0InMKfdbii971CeUY0qk+1ad7Fn9txuR5omttkEtM9Hh9Afz1kGxa4ia9+d71OV4KoXVykqr/bD284rhOooX4/mU= vincenzo@dracula"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJtziDwyaqKfBPL1dDEM6kMdA+KTL+d0810PzAbOsWHn kyrill@kyrill-ThinkPad-T495"
     ];
-
   };
 
   environment.systemPackages = with pkgs; [
@@ -95,7 +127,6 @@
       settings.PasswordAuthentication = false;
       settings.KbdInteractiveAuthentication = false;
       settings.PermitRootLogin = "no";
-
     };
     fail2ban.enable = true;
     # gitlab needs nginx proxy
@@ -121,8 +152,7 @@
       virtualHosts."git.dumustbereitsein.de" = {
         enableACME = true;
         forceSSL = true;
-        locations."/".proxyPass =
-          "http://unix:/run/gitlab/gitlab-workhorse.socket";
+        locations."/".proxyPass = "http://unix:/run/gitlab/gitlab-workhorse.socket";
       };
     };
     gitlab = {
@@ -153,13 +183,11 @@
           email_reply_to = "gitlab-no-reply@dumustbereitsein.de";
         };
       };
-#      registry = {
-#        enable = true;
-#        externalPort = 5050;
-#        externalAddress = "registry.dumustbereitsein.de";
-#        certFile = "/var/cert/registry.crt";
-#        keyFile = "/var/cert/registry.key";
-#      };
+      #      registry = {
+      #        enable = true;
+      #        certFile = "/var/cert/registry.crt";
+      #        keyFile = "/var/cert/registry.key";
+      #      };
     };
 
     gitlab-runner = {
@@ -167,8 +195,11 @@
       gracefulTermination = true;
     };
 
+    dockerRegistry = {
+      enable = true;
+      openFirewall = true;
+    };
     syncthing = {
-
       enable = true;
       user = "vincenzo";
       openDefaultPorts = true;
@@ -177,33 +208,27 @@
       settings = {
         devices = {
           "dracula" = {
-            id =
-              "QGVRLBK-OZX7PIM-JMGKYHF-KSFI5VS-FJH6RGI-4YGB6H6-EYAJ27S-TM5LTQ6";
+            id = "QGVRLBK-OZX7PIM-JMGKYHF-KSFI5VS-FJH6RGI-4YGB6H6-EYAJ27S-TM5LTQ6";
             autoAcceptFolders = true;
           };
           "valnar" = {
-            id =
-              "RUL7KJT-FSXO3VI-QP76KVO-LAY5CYT-NERMG3C-EY3TYUU-FQDDKG5-NJ5V4AS";
+            id = "RUL7KJT-FSXO3VI-QP76KVO-LAY5CYT-NERMG3C-EY3TYUU-FQDDKG5-NJ5V4AS";
             autoAcceptFolders = true;
           };
           "asgar" = {
-            id =
-              "H5KT7VJ-OFUH3PD-6HH4KRI-VKOOOMY-Z5KNIQL-DVTBCAN-PQ44EAY-AAAS7AO";
+            id = "H5KT7VJ-OFUH3PD-6HH4KRI-VKOOOMY-Z5KNIQL-DVTBCAN-PQ44EAY-AAAS7AO";
             autoAcceptFolders = true;
           };
           "kyrill-thinkpad-t495" = {
-            id =
-              "PERVZSH-ZPN5MET-MM6ADZG-TTDUOV5-USGIDY3-4K4UT6N-JPUCYGM-6PIIWQQ";
+            id = "PERVZSH-ZPN5MET-MM6ADZG-TTDUOV5-USGIDY3-4K4UT6N-JPUCYGM-6PIIWQQ";
             autoAcceptFolders = true;
           };
           "kyrill-handy" = {
-            id =
-              "VKWKKOP-4ZNV7AY-MSDT3EA-LOJQFAS-BJ7MPWT-ZJ353LV-7HRXYA3-3763VAH";
+            id = "VKWKKOP-4ZNV7AY-MSDT3EA-LOJQFAS-BJ7MPWT-ZJ353LV-7HRXYA3-3763VAH";
             autoAcceptFolders = true;
           };
           "kyrill-tablet" = {
-            id =
-              "T7SVLB6-ZWQM7DO-ZDULZNB-I6QGSQZ-JJUFPO2-7N3GVP7-HONELH2-3GDJSAE";
+            id = "T7SVLB6-ZWQM7DO-ZDULZNB-I6QGSQZ-JJUFPO2-7N3GVP7-HONELH2-3GDJSAE";
             autoAcceptFolders = true;
           };
         };
@@ -211,7 +236,11 @@
           "default" = {
             id = "default";
             path = "/home/vincenzo/default";
-            devices = [ "kyrill-handy" "kyrill-tablet" "kyrill-thinkpad-t495" ];
+            devices = [
+              "kyrill-handy"
+              "kyrill-tablet"
+              "kyrill-thinkpad-t495"
+            ];
             type = "sendreceive";
             enable = true;
             versioning = {
@@ -222,7 +251,11 @@
           "documents" = {
             id = "baqfs-svyhe";
             path = "/home/vincenzo/documents";
-            devices = [ "dracula" "asgar" "valnar" ];
+            devices = [
+              "dracula"
+              "asgar"
+              "valnar"
+            ];
             type = "sendreceive";
             enable = true;
             versioning = {
@@ -233,7 +266,11 @@
           "org" = {
             id = "cccdk-miidx";
             path = "/home/vincenzo/org";
-            devices = [ "dracula" "asgar" "valnar" ];
+            devices = [
+              "dracula"
+              "asgar"
+              "valnar"
+            ];
             type = "sendreceive";
             enable = true;
             versioning = {
@@ -244,7 +281,11 @@
           "code" = {
             id = "wnku3-6n7g5";
             path = "/home/vincenzo/code";
-            devices = [ "dracula" "asgar" "valnar" ];
+            devices = [
+              "dracula"
+              "asgar"
+              "valnar"
+            ];
             type = "sendreceive";
             enable = true;
             versioning = {
@@ -255,7 +296,10 @@
           "amiconsult" = {
             id = "amiconsult";
             path = "/home/vincenzo/amiconsult";
-            devices = [ "dracula" "valnar" ];
+            devices = [
+              "dracula"
+              "valnar"
+            ];
             type = "sendreceive";
             enable = true;
             versioning = {
@@ -320,5 +364,4 @@
   security.acme.acceptTerms = true;
   security.acme.defaults.email = "vincenzo.pace94@icloud.com";
   system.stateVersion = "23.05";
-
 }
