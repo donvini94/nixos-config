978552660f9f47a424e4f870449dec0274fc4b99
Vincenzo Pace <vincenzo.pace94@icloud.com>
2024-10-29 11:55:02 +0100

    add flakes support globally

--- hosts/desktop/common.nix
+++ hosts/desktop/common.nix
@@ -40,6 +40,10 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   nix = {
+    settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
     settings.trusted-users = [ "${username}" ];
     settings.auto-optimise-store = true;
     optimise.automatic = true;
