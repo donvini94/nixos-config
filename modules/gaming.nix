4a5ea36039b55cdeb09d84c46aa67f93736206dc
Vincenzo Pace <vincenzo.pace94@icloud.com>
2025-05-04 11:08:25 +0200

    better gaming

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -10,11 +10,15 @@
     steam = {
       enable = true;
       gamescopeSession.enable = true;
+      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+
     };
     gamescope.enable = true;
   };
   programs.gamemode.enable = true;
-
+  hardware.xone.enable = true; # support for the xbox controller USB dongle
   environment.systemPackages = with pkgs; [
     mangohud
     protonup
